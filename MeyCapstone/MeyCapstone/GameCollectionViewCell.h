13dec7e963bac806a5b3b677f49fbf7a99a0c9be
Ellen Mey <eisforellen@gmail.com>
2016-07-22 14:28:43 -0400

    Add collection view and custom cell class

---
+++ MeyCapstone/MeyCapstone/GameCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  GameCollectionViewCell.h
+//  MeyCapstone
+//
+//  Created by Ellen Mey on 7/22/16.
+//  Copyright © 2016 Ellen Mey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ MeyCapstone/MeyCapstone/GameCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  GameCollectionViewCell.m
+//  MeyCapstone
+//
+//  Created by Ellen Mey on 7/22/16.
+//  Copyright © 2016 Ellen Mey. All rights reserved.
+//
+
+#import "GameCollectionViewCell.h"
+
+@implementation GameCollectionViewCell
+
+@end
