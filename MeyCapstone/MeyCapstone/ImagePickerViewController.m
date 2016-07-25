f8291cc409933f7b4198fc29f6f620137e2ac446
Ellen Mey <eisforellen@gmail.com>
2016-07-25 11:27:44 -0400

    Add ImagePickerVC and Buttons

---
+++ MeyCapstone/MeyCapstone/ImagePickerViewController.m
@@ -0,0 +1,64 @@
+//
+//  ImagePickerViewController.m
+//  MeyCapstone
+//
+//  Created by Ellen Mey on 7/25/16.
+//  Copyright © 2016 Ellen Mey. All rights reserved.
+//
+
+#import "ImagePickerViewController.h"
+
+@interface ImagePickerViewController ()
+
+@end
+
+@implementation ImagePickerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)takePhoto:(id)sender {
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Yikes!" message:@"There is no camera :(" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){
+            [alert dismissViewControllerAnimated:YES completion:nil];
+        }];
+        [alert addAction:ok];
+        [self presentViewController:alert animated:YES completion:nil];
+    } else {
+        UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+        picker.delegate = self;
+        picker.allowsEditing = YES;
+        picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        
+        [self presentViewController:picker animated:YES completion:nil];
+    }
+}
+- (IBAction)selectPhoto:(id)sender {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:picker animated:YES completion:nil];
+}
+- (IBAction)submitImage:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
